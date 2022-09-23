103f817b104be8f415fda0c58008c0b351318348
João Lucas <zavarash@outlook.com>
2022-09-22 23:11:19 -0300

    eventos de touch adicionados

--- js/slide.js
+++ js/slide.js
@@ -16,24 +16,35 @@ export default class Slide {
   }
 
   onStart(event) {
-    event.preventDefault();
-    this.wrapper.addEventListener('mousemove', this.onMove);
-    this.distance.startX = event.clientX;
+    let moveType;
+    if (event.type === 'mousedown') {
+      event.preventDefault();
+      this.distance.startX = event.clientX;
+      moveType = 'mousemove';
+    } else {
+      this.distance.startX = event.changedTouches[0].clientX;
+      moveType = 'touchmove';
+    }
+    this.wrapper.addEventListener(moveType, this.onMove);
   }
 
   onMove(event) {
-    const finalPosition = this.updatePosition(event.clientX);
+    const pointerPosition = event.type === 'mousemove' ? event.clientX : event.changedTouches[0].clientX;
+    const finalPosition = this.updatePosition(pointerPosition);
     this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
-    this.wrapper.removeEventListener('mousemove', this.onMove);
+    const moveType = event.type === 'mouseup' ? 'mousemove' : 'touchmove';
+    this.wrapper.removeEventListener(moveType, this.onMove);
     this.distance.finalPosition = this.distance.movePosition;
   }
 
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('touchstart', this.onStart);
     this.wrapper.addEventListener('mouseup', this.onEnd);
+    this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
   bindEvents() {
